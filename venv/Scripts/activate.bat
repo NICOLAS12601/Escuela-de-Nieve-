5041e1188119d7d9ab7b46e64f52fc14077b6750
mathiaspteliz <mathiasperez042@gmail.com>
2024-11-17 20:45:59 -0300

    aplicacion funcionando

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\Matias\Desktop\escuela_nieve\venv
+set VIRTUAL_ENV=C:\Repositorio\Escuela-de-Nieve-\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
